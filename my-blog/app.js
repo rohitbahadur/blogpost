5bed0ed7ce5bb7297ed1d9b98f4575fb19f95ee0
rbahadur <rohit.bahadur@diva-e.com>
2022-12-09 01:50:44 +0100

    add bootstrap

--- my-blog/app.js
+++ my-blog/app.js
@@ -27,6 +27,15 @@ app.get("/contact", function (req, res) {
   res.render("contact", { contactContent: contactContent })
 });
 
+app.get("/compose", function (req, res) {
+  res.render("compose")
+})
+
+app.post("/compose", function (req, res) {
+  console.log(req.body.postTitle)
+
+})
+
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
